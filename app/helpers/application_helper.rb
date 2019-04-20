2ea6b0208bce083d3dc3db92aae8edbe524fc78d
Ilona Sellenberkova <ilona.sell@gmail.com>
2019-04-20 17:05:12 +0200

    Helper to display avatar when login (gravatar)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def avatar_url(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    "https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identical&s=150"
+  end
 end
+
+
+# taking user object -> his email, convert to "string of numbers" (MD5)
