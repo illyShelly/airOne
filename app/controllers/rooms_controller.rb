380c9d4973a0641023d51b0bd7a22b65204e8478
Ilona Sellenberkova <ilona.sell@gmail.com>
2019-04-23 23:00:39 +0200

    Room View -> upgrade colours, CSS forms, FIX - flash messages

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -13,11 +13,16 @@ class RoomsController < ApplicationController
 
   def create
     @room = current_user.rooms.build(room_params)
-    if room.save
       # rooms/id/listings
-      redirect_to listing_room_path(@room), notice: "Saved successfully"
+    if @room.save
+      # flash[:notice] = "Saved successfully"
+      # redirect_to listing_room_path(@room)
+      redirect_to listing_room_path(@room), flash: { notice: "Saved successfully" }
     else
-      render :new, alert: "Something went wrong..."
+      flash[:alert] = "Something went wrong ..."
+      # render :new, alert: "Something went wrong..." does not work
+      render :new
+      # render :new, :flash => { :alert => "Failed to delete!" }
     end
   end
 
@@ -45,10 +50,10 @@ class RoomsController < ApplicationController
   def update
     if @room.update(room_params)
       # redirect_to :back, notice: "Saved sucessfully"
-      flash[:notice] = "Saved sucessfully"
+      flash["notice"] = "Saved sucessfully"
       # redirect_to 'room_listing_path'
     else
-      flash[:notice] = "Wasn't updated"
+      flash["notice"] = "Wasn't updated"
     end
     # redirect back to the current page
     redirect_back(fallback_location: request.referer)
@@ -62,6 +67,10 @@ class RoomsController < ApplicationController
 
   def room_params
     params.require(:room).permit(:home_type, :room_type, :accommodate, :bed_room, :bath_room, :listing_name, :summary, :address, :is_tv, :is_kitchen, :is_air, :is_heating, :is_internet, :price, :active)
-
   end
 end
+
+# two ways of writing flash info, if combine with redirect_to => run error
+# no implicit conversion of Symbol into String
+# render :new, alert: "Something went wrong..." DO NOT WORK
+# render :new, :flash => { :alert => "Failed to save!" }
