d6ec4b56d594939ceb0d70b1534b4a27640ee536
Ilona Sellenberkova <ilona.sell@gmail.com>
2019-04-23 13:12:26 +0200

    Profile page - Registrations controller back to it -> new password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,10 +7,11 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:fullname])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:fullname])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:fullname, :phone_number, :description, :password, :password_confirmation])
 
   end
 end
 
 
 # devise gem doc - there are 2 places (forms to update)
+# update parameters for phone and description
