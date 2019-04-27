a19675ce6306825d337d984b3df5dc5e1f2b6920
Ilona Sellenberkova <ilona.sell@gmail.com>
2019-04-27 19:15:09 +0200

    View room index - method for img Room model

--- app/models/room.rb
+++ app/models/room.rb
@@ -8,9 +8,18 @@ class Room < ApplicationRecord
   validates :bath_room, presence: true
 
   has_many_attached :images
+
+  def cover_photo(size)
+    if self.images.attached? && self.images[0].variable?
+      self.images[0].variant(size)
+    else
+      "placeholder.jpg"
+    end
+  end
 end
 
 # 1. in listing -> I manage above validations
 # 2. in form I require when updating: pricing, description, location(address), photo -> later on
 # -> amenities (does not need to be part of equipment (like TV, heating)) => not there
 # 3. using Active-storage (storage_blobs, storage_attachment(jointable)) = adding multiple images that will be associated with each room
+# 4. cover_photo in index.html, attached from active storage
