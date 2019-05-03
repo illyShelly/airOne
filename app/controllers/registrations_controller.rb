7dc8b3f3f320c6adda23fac5b1774d5899aad795
Ilona Sellenberkova <ilona.sell@gmail.com>
2019-05-03 13:01:46 +0200

    RegistrationsController - fix update password

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,9 +1,12 @@
 class RegistrationsController < Devise::RegistrationsController
   protected
   def update_resource(resource, params)
+    if params[:password]
+      resource.password = params[:password]
+      resource.password_confirmation = params[:password_confirmation]
+    end
     resource.update_without_password(params)
   end
 end
-
 
 # needs to be fixed -> do not update
